afa32ae30debee0050107fc9b6ace54cdc5b849d
MohammedYaseenRon <yaseenron3@gmail.com>
2024-08-20 11:29:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+
+document.querySelector('.nav-toggle').addEventListener('click',function(){
+    document.querySelector('.nav-list').classList.toggle('active');
+});
+
+// -----------reservation-------------
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('reservation-form');
+    const submitButton = document.querySelector('.submit-btn');
+
+    submitButton.addEventListener('click', (event) => {
+        event.preventDefault(); 
+
+        const name = document.getElementById('name').value.trim();
+        const phone = document.getElementById('phone').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const date = document.getElementById('date').value;
+        const time = document.getElementById('time').value;
+        const guest = document.getElementById('guest').value.trim();
+
+     
+        if (name && phone && email && date && time && guest) {
+            alert('Reservation Submitted Successfully!\n' +
+                `Name: ${name}\n` +
+                `Phone: ${phone}\n` +
+                `Email: ${email}\n` +
+                `Date: ${date}\n` +
+                `Time: ${time}\n` +
+                `Number of Guests: ${guest}\n`);
+
+            
+            form.reset();
+        } else {
+            alert("Please fill in all the fields.");
+        }
+    });
+});
+
+
+// -------------------------contact---------------------------------
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const form = document.querySelector('.contact-form');
+    
+
+    form.addEventListener('submit',function event() {
+        const name = document.getElementById('contact-name').value.trim();
+        const phone = document.getElementById('phone').value.trim();
+        const message = document.getElementById('message').value.trim();
+    
+
+        if (!name || !phone || !message) {
+            alert('please fill in all the fiels');
+            event.preventDefault();
+            return;
+        }
+        
+        alert("Form submitted successfully");
+
+    }); 
+       
+}); 
+
